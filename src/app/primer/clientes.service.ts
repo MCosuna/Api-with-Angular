7bcb0f99486f6674c370e83e3f4d8cb41d467c6b
Maria del carmen Osuna <34645403+MCosuna@users.noreply.github.com>
2022-11-15 15:23:58 +0100

    Update clientes.service.ts

--- src/app/primer/clientes.service.ts
+++ src/app/primer/clientes.service.ts
@@ -10,7 +10,7 @@ todo;
     this.http.get('https://jsonplaceholder.typicode.com/todos').subscribe(
       // this.http.get('https://newsapi.org/v2/top-headlines?' +
       // 'country=us&' +
-      // 'apiKey=8e0121aa4ee249018a70ff28eb5a0c6e').subscribe(
+      // 'apiKey=YOUR-API-KEY').subscribe(
 
     resultado => {
       // console.log(JSON.stringify(this.todo))
